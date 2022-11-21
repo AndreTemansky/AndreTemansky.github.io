b7950504253f024a3ab1dc0edf24b876700151e2
AndréTemansky.github.io <106269481+AndreTemansky@users.noreply.github.com>
2022-11-20 22:33:07 -0300

    Add files via upload

---
+++ principal.js
@@ -0,0 +1,39 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent="Aparecida Nutricionista";
+
+var paciente = document.querySelectorAll(".paciente");
+
+for(var i = 0; i < paciente.length ; i++){
+
+  var paciente = paciente[i];
+
+  var tdPeso = paciente.querySelector(".info-peso");
+  var peso = tdPeso.textContent;
+
+  var tdAltura= paciente.querySelector(".info-altura");
+  var altura = tdAltura.textContent;
+
+  var tdImc = paciente.querySelector(".info-imc");
+
+  var pesoEhValido=true;
+  var alturaEhValida=true;
+
+  if(peso <= 0 || peso >= 1000){
+    console.log("Peso inválido!")
+    pesoEhValido=false;
+    tdImc.textContent="Peso inválido!"
+    paciente.classList.add("paciente inválido");
+  }
+
+  if(altura <= 0 || altura >= 3.00){
+    console.log ("Altura inválida!")
+    alturaEhValida = false;
+    tdImc.textContent="Altura inválida!"
+    paciente.classList.add ("paciente inválido")
+  }
+
+  if(alturaEhValida && pesoEhValida){
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc.toFixed(2);
+  }
+}
